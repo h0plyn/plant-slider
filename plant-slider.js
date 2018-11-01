80ddfb764ba502f2f41d266d88c30f56058165ac
Garrett Haas <garretthaas@users.noreply.github.com>
2018-11-01 16:50:25 -0700

    Added number of slides reference as dots

--- plant-slider.js
+++ plant-slider.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
-	$('.plant-slider .slide').each(function() {
-		$(this).find('img:first').fadeIn();
-	});
-	(function(){
+	$('.plant-slider').each(function() {
+		$(this).find('img:first').addClass('ps-active');
+		var n = $(this).find('img').length,
+			a = n;
+		if (n > 1) {
+			for (var i = 0; i < n; i++) {
+				$(this).children("ul.plant-slider-ui").prepend('<li></li>');
+				a = a - 1;
+			}
+		} else {
 
-	var ps 	= $('.plant-slider'),
-			i 	= 0;
-	ps.on('click', function(e){
+		}
+	});
+
+	(function() {
+
+		var ps = $('.plant-slider'),
+			i = 0;
+		ps.on('click', function(e) {
+
 			e.preventDefault();
-			var s = $(this).find('img')
-	    i = (i + 1) % s.length;
-	    s.hide().eq(i).show();
-	});
-
+			var s = $(this).find('img'),
+				ui = $(this).find('li');
+			i = (i + 1) % s.length;
+			s.removeClass('ps-active').eq(i).addClass('ps-active');
+			ui.removeClass('ps-active').eq(i).addClass('ps-active');
+		});
 	})();
 });
