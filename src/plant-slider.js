7887dc7d419e690a0a5e5a0a5fc17ad3e5d29cfe
Garrett Haas <garretthaas@users.noreply.github.com>
2018-11-01 18:56:02 -0700

    minified, styled html example

---
+++ src/plant-slider.js
@@ -0,0 +1,28 @@
+(function($) {
+  $.fn.plantSlider = function() {
+    this.each(function() {
+      $(this).find('img:first').addClass('ps-active');
+      var n = $(this).find('img').length,
+        a = n;
+      if (n > 1) {
+        for (var i = 0; i < n; i++) {
+          $(this).children("ul.plant-slider-ui").prepend('<li></li>');
+          a = a - 1;
+        }
+        $(this).find("li:first").addClass('ps-active');
+      } else {
+
+      }
+    });
+    var  i = 0;
+    this.on('click', function(e) {
+
+      e.preventDefault();
+      var s = $(this).find('img'),
+        ui = $(this).find('li');
+      i = (i + 1) % s.length;
+      s.removeClass('ps-active').eq(i).addClass('ps-active');
+      ui.removeClass('ps-active').eq(i).addClass('ps-active');
+    });
+  }
+}(jQuery));
