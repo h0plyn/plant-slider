735f7a7e9bb57d33a57b54a6e0f1ecd7ef8d653e
Garrett Haas <garretthaas@users.noreply.github.com>
2018-11-01 17:26:44 -0700

    converted to plugin

--- plant-slider.js
+++ plant-slider.js
@@ -1,30 +1,27 @@
-$(document).ready(function() {
-	$('.plant-slider').each(function() {
-		$(this).find('img:first').addClass('ps-active');
-		var n = $(this).find('img').length,
-			a = n;
-		if (n > 1) {
-			for (var i = 0; i < n; i++) {
-				$(this).children("ul.plant-slider-ui").prepend('<li></li>');
-				a = a - 1;
-			}
-		} else {
+(function($) {
+  $.fn.plantSlider = function() {
+    this.each(function() {
+      $(this).find('img:first').addClass('ps-active');
+      var n = $(this).find('img').length,
+        a = n;
+      if (n > 1) {
+        for (var i = 0; i < n; i++) {
+          $(this).children("ul.plant-slider-ui").prepend('<li></li>');
+          a = a - 1;
+        }
+      } else {
 
-		}
-	});
+      }
+    });
+    var  i = 0;
+    this.on('click', function(e) {
 
-	(function() {
-
-		var ps = $('.plant-slider'),
-			i = 0;
-		ps.on('click', function(e) {
-
-			e.preventDefault();
-			var s = $(this).find('img'),
-				ui = $(this).find('li');
-			i = (i + 1) % s.length;
-			s.removeClass('ps-active').eq(i).addClass('ps-active');
-			ui.removeClass('ps-active').eq(i).addClass('ps-active');
-		});
-	})();
-});
+      e.preventDefault();
+      var s = $(this).find('img'),
+        ui = $(this).find('li');
+      i = (i + 1) % s.length;
+      s.removeClass('ps-active').eq(i).addClass('ps-active');
+      ui.removeClass('ps-active').eq(i).addClass('ps-active');
+    });
+  }
+}(jQuery));
